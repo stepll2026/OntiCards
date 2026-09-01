4c3c6edf4027c0eab8ce5004faf6002340cadef3
Ggxiaohan <849631113@qq.com>
2026-09-01 16:54:45 +0800

    -) 前端模块：剔除冗余的代码文件

--- OntiCards_Web/app/[lng]/(newLayout)/ask/page.tsx
+++
@@ -1,6 +0,0 @@
-import AskPage from './AskPage';
-import type { Props } from './type';
-
-export default function Page(_props: Props) {
-  return <AskPage />;
-}
--- OntiCards_Web/app/[lng]/(newLayout)/ask/type.ts
+++
@@ -1,6 +0,0 @@
-export type Props = {
-  params: {
-    lng: string;
-  };
-};
-
